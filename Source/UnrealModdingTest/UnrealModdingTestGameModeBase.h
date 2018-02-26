592df2755c041d1fd86b28e0106d9c30de99095b
Calem Bendell <calem.j.bendell@gmail.com>
2018-02-26 15:59:53 -0500

    add error state

---
+++ Source/UnrealModdingTest/UnrealModdingTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealModdingTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMODDINGTEST_API AUnrealModdingTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
